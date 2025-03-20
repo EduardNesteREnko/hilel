d77ff511e84342f1bd7d1a6bb2643c70ae9562fb
EduardNesteREnko <nasviratusforchen@gmail.com>
2025-03-20 12:41:26 +0200

    Update dz_2.2.py

--- dz_2.2.py
+++ dz_2.2.py
@@ -1,11 +1,11 @@
 x = float(input('Type number:'))
 
-1 == x // 1000
-2 == x //100
-3 == x //10
-4 == x //1
+number1 == x // 1000
+number2 == x //100
+number3 == x //10
+number4 == x //1
 
-print(1)
-print(2)
-print(3)
-print(4)
+print(number1)
+print(number2)
+print(number3)
+print(number4)

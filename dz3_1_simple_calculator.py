17409278cbccab1c20f06f9f629452adbb2ff0ac
EduardNesteREnko <nasviratusforchen@gmail.com>
2025-03-27 11:37:21 +0200

    Add files via upload

---
+++ dz3_1_simple_calculator.py
@@ -0,0 +1,11 @@
+a = int(input("First number"))
+b = int(input("Second number"))
+action = input("Action")
+if action == "+":
+    print(a + b)
+elif action == "-":
+    print(a - b)
+elif action == "*":
+    print(a * b)
+elif action == "/":
+    print(a / b)

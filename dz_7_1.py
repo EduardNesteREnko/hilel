35b979bcaec20662a8d2f010d875bd5cbdbdc44f
EduardNesteREnko <nasviratusforchen@gmail.com>
2025-04-25 21:11:18 +0300

    Add files via upload

---
+++ dz_7_1.py
@@ -0,0 +1,10 @@
+name = input(str('Put your name pls:'))
+age = input(str('Put your age pls:'))
+
+def say_hi(name, age):
+  say_hello = f"Hi. My name is {name} and I'm {age} years old"
+  return say_hello
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
